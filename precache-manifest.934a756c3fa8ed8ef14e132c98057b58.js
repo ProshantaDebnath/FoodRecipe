41a5517f0019688daf716326cc25b217ec587d90
Proshanta Debnath <73601711+ProshantaDebnath@users.noreply.github.com>
2021-10-29 23:11:59 +0530

    Updates

---
+++ precache-manifest.934a756c3fa8ed8ef14e132c98057b58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b95cc04699f0bf8022d1a14fd511e0e5",
+    "url": "/FoodRecipe/index.html"
+  },
+  {
+    "revision": "f668134c6074b1595d85",
+    "url": "/FoodRecipe/static/css/main.46802781.chunk.css"
+  },
+  {
+    "revision": "545f096040c0984faff0",
+    "url": "/FoodRecipe/static/js/2.7e0c2259.chunk.js"
+  },
+  {
+    "revision": "f668134c6074b1595d85",
+    "url": "/FoodRecipe/static/js/main.5b0eb1c8.chunk.js"
+  },
+  {
+    "revision": "5d6fb21915d26e5987d7",
+    "url": "/FoodRecipe/static/js/runtime-main.a19ccead.js"
+  }
+]);
